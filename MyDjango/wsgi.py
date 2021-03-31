f23cd0271fd376191dc04d8a15823d399022b5df
Ambitioner-c <656359504@qq.com>
2021-03-31 14:56:43 +0800

    First commit

---
+++ MyDjango/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for MyDjango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+import sys
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyDjango.settings')
+
+application = get_wsgi_application()
+sys.path.append('/home/admin/Projects/DjangoProjects/MyDjango/')
